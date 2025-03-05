81c99850431b6a5ab5cc42ded0424649a37b2c5f
Martín Ayala <133398040+MartinXde23@users.noreply.github.com>
2025-03-05 04:52:22 -0500

    Configuracion despliegue

--- src/Database.js
+++ src/Database.js
@@ -6,7 +6,7 @@ mongoose.set('strictQuery', true)
 
 const connection = async () => {
     try{
-        const {connection} = await mongoose.connect(process.env.MONGOURIPRODUCTION)
+        const {connection} = await mongoose.connect(process.env.MONGODBURILOCAL)
         console.log(`Database is connected on ${connection.host} - ${connection.post}`)
     }catch (error){
         console.log(error)
